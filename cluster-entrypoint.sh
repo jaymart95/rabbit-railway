893bd1a8ed3d94adaa8459b469736892b4954d24
Jason Martin <45865838+jaymart95@users.noreply.github.com>
2024-06-27 22:41:02 +0100

    Update cluster-entrypoint.sh

--- cluster-entrypoint.sh
+++ cluster-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Change .erlang.cookie permission
-chmod 400 /var/lib/rabbitmq/.erlang.cookie
+chmod 400 /usr/var/lib/rabbitmq/.erlang.cookie
 
 # Get hostname from environment variable
 HOSTNAME=$(hostname)
@@ -12,11 +12,11 @@ echo "Starting RabbitMQ Server For host: " $HOSTNAME
 if [ -z "$JOIN_CLUSTER_HOST" ]; then
     /usr/local/bin/docker-entrypoint.sh rabbitmq-server &
     sleep 10
-    rabbitmqctl wait --timeout 30000 /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
+    rabbitmqctl wait --timeout 30000 /usr/var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
 else
     /usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
     sleep 10
-    rabbitmqctl wait --timeout 30000 /var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
+    rabbitmqctl wait --timeout 30000 /usr/var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
     rabbitmqctl stop_app
     rabbitmqctl join_cluster rabbit@$JOIN_CLUSTER_HOST
     rabbitmqctl start_app
