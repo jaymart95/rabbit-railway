f51a53358401d9fe517375fe5b99fa9ad6221ccc
Jason Martin <45865838+jaymart95@users.noreply.github.com>
2024-06-28 00:41:49 +0100

    Update cluster-entrypoint.sh

--- cluster-entrypoint.sh
+++ cluster-entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 chmod 400 /usr/var/lib/rabbitmq/.erlang.cookie
 
 # Get hostname from environment variable
-HOSTNAME=rabbitmq1.railway.internal
+HOSTNAME=$(hostname)
 echo "Starting RabbitMQ Server For host: " $HOSTNAME
 
 if [ -z "$JOIN_CLUSTER_HOST" ]; then
