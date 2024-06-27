88167778c812d022ad81f987cb174ec190e46a7c
Jason Martin <45865838+jaymart95@users.noreply.github.com>
2024-06-28 00:13:34 +0100

    Update cluster-entrypoint.sh

--- cluster-entrypoint.sh
+++ cluster-entrypoint.sh
@@ -3,29 +3,22 @@
 set -e
 
 # Change .erlang.cookie permission
-chmod 400 /var/lib/rabbitmq/.erlang.cookie
+chmod 400 /usr/var/lib/rabbitmq/.erlang.cookie
 
-# Get the full hostname
+# Get hostname from environment variable
 HOSTNAME=$(hostname)
-echo "Starting RabbitMQ Server for host: $HOSTNAME"
+echo "Starting RabbitMQ Server For host: " $HOSTNAME
 
 if [ -z "$JOIN_CLUSTER_HOST" ]; then
     /usr/local/bin/docker-entrypoint.sh rabbitmq-server &
-    sleep 30
-    rabbitmqctl wait --timeout 60000 /var/lib/rabbitmq/mnesia/rabbit@$HOSTNAME.pid
+    sleep 10
+    rabbitmqctl wait --timeout 30000 /usr/var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
 else
     /usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
-    sleep 30
-    rabbitmqctl wait --timeout 60000 /var/lib/rabbitmq/mnesia/rabbit@$HOSTNAME.pid
-    echo "Stopping RabbitMQ application on node $HOSTNAME..."
+    sleep 10
+    rabbitmqctl wait --timeout 30000 /usr/var/lib/rabbitmq/mnesia/rabbit\@$HOSTNAME.pid
     rabbitmqctl stop_app
-    echo "Joining node $HOSTNAME to cluster $JOIN_CLUSTER_HOST..."
-    rabbitmqctl join_cluster $JOIN_CLUSTER_HOST
-    if [ $? -ne 0 ]; then
-        echo "Error joining cluster. Exiting."
-        exit 1
-    fi
-    echo "Starting RabbitMQ application on node $HOSTNAME..."
+    rabbitmqctl join_cluster rabbit@$JOIN_CLUSTER_HOST
     rabbitmqctl start_app
 fi
 
